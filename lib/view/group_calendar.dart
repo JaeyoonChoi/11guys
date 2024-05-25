93557510fcd0541f280b5a5f09b4aa81897372f1
JunSik <zlwm9951@gmail.com>
2024-05-25 09:56:17 +0900

    group_calendar.dart 부분만 수정했습니다

--- lib/view/group_calendar.dart
+++ lib/view/group_calendar.dart
@@ -1,26 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
-class GroupCalendar extends StatelessWidget {
-  final String username; // 사용자 ID 추가
+void main() => runApp(GroupCalendar(username: '사용자명'));
 
-  GroupCalendar({required this.username}); // 생성자 수정
+class GroupCalendar extends StatefulWidget {
+  final String username;
+
+  GroupCalendar({required this.username});
+
+  @override
+  _GroupCalendarState createState() => _GroupCalendarState();
+}
+
+class _GroupCalendarState extends State<GroupCalendar> {
+  List<Map<String, dynamic>> groups = []; // 여러 데이터 타입을 저장하기 위해 dynamic 사용
+  //여기에 그룹명, 인원 수, 학번 입력한 리스트가 저장되어있음
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // appBar: ,
-        body: Text('시간매칭', style: TextStyle(fontFamily: 'HancomMalangMalang',),),
-        floatingActionButton: floatingButtons(),
+        appBar: AppBar(
+          title: Text('그룹 캘린더'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: groups.isEmpty
+                      ? Text('그룹이 없습니다')
+                      : DataTable(
+                    headingRowHeight: 56.0,
+                    dataRowHeight: 64.0,
+                    columns: const <DataColumn>[
+                      DataColumn(label: Text('그룹명', style: TextStyle(fontSize: 16))),
+                      DataColumn(label: Text('인원수', style: TextStyle(fontSize: 16))), // 인원 수 열  (그룹명만 넣기 허전해서 일단 만들어 놓음)
+                      DataColumn(label: Text('인원 추가', style: TextStyle(fontSize: 16))), // 인원 추가 열
+                      DataColumn(label: Text('그룹 삭제', style: TextStyle(fontSize: 16))), // 삭제 열 추가
+                    ],
+                    rows: groups.map<DataRow>((group) {
+                      return DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(group['name'], style: TextStyle(fontSize: 14))),
+                          DataCell(Text(group['count'], style: TextStyle(fontSize: 14))),
+                          DataCell(IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {
+                              _showAddMemberDialog(group['name']);
+                            },
+                          )),
+                          DataCell(IconButton( // 삭제 버튼 추가
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              _deleteGroup(group['name']);
+                            },
+                          )),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: floatingButtons(context),
       ),
     );
   }
-}
 
-// FAB
-Widget floatingButtons() {
-  return SpeedDial(
+
+  Widget floatingButtons(BuildContext context) {
+    return SpeedDial(
       animatedIcon: AnimatedIcons.menu_close,
       visible: true,
       curve: Curves.bounceIn,
@@ -28,16 +85,123 @@ Widget floatingButtons() {
       children: [
         SpeedDialChild(
             child: const Icon(Icons.group, color: Colors.white),
-            label: "그룹 만들기",
+            label: "그룹 캘린더",
             labelStyle: const TextStyle(
-                color: Colors.white,
-                fontSize: 13.0,
-                fontFamily: 'HancomMalangMalang',
-                fontWeight: FontWeight.w100), // font 굵기 조정
+                color: Colors.white, fontSize: 13.0, fontFamily: 'HancomMalangMalang', fontWeight: FontWeight.w100),
             backgroundColor: Colors.green,
             labelBackgroundColor: Colors.green,
-            onTap: () {} // onTap이 눌렀을 때 실행되는 거
-        ),
-      ]
-  );
+            onTap: () {
+              _showCreateGroupDialog(context);
+            }),
+      ],
+    );
+  }
+
+  void _showCreateGroupDialog(BuildContext context) {
+    TextEditingController groupNameController = TextEditingController();
+    TextEditingController memberCountController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('그룹 캘린더'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: groupNameController,
+                  decoration: InputDecoration(hintText: "그룹명"),
+                ),
+                TextField(
+                  controller: memberCountController,
+                  decoration: InputDecoration(hintText: "인원 수"),
+                  keyboardType: TextInputType.number,
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('취소'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('확인'),
+              onPressed: () {
+                setState(() {
+                  groups.add({
+                    "name": groupNameController.text,
+                    "count": memberCountController.text,
+                    "members": [] // 멤버 ID를 저장하기 위한 리스트 추가
+                  });
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showAddMemberDialog(String groupName) {
+    List<TextEditingController> controllers = [TextEditingController()];
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('인원 추가'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    for (var controller in controllers)
+                      TextField(
+                        controller: controller,
+                        decoration: InputDecoration(hintText: "학번"),
+                        keyboardType: TextInputType.number,
+                      ),
+                    TextButton(
+                      child: Text('인원 추가'),
+                      onPressed: () {
+                        setState(() {
+                          controllers.add(TextEditingController());
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('취소'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('확인'),
+                  onPressed: () {
+                    // 여기에 학번 추가 로직을 구현하세요.
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _deleteGroup(String groupName) {
+    setState(() {
+      groups.removeWhere((group) => group['name'] == groupName);
+    });
+  }
 }
