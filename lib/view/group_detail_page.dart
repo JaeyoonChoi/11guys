c5e870a49655424f53c4a3871118a98335b9fd5c
Jaeyoon <jeyoonchoi03@gmail.com>
2024-06-01 02:40:59 +0900

    # 페이지 순서 변경 # 아이콘 색상 변경
    # group_detail_page에 weekview 추가

--- lib/view/group_detail_page.dart
+++ lib/view/group_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class GroupDetailedPage extends StatelessWidget {
   final String pin;
@@ -9,15 +10,19 @@ class GroupDetailedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('그룹 캘린더 상세'),
+        title: Row(
+          children: [
+            Text('그룹 캘린더 상세'),
+            SizedBox(width: 8.0),
+            Text('            PIN 번호: $pin', style: TextStyle(fontSize: 20)),
+          ],
+        )
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          'PIN 번호: $pin',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+      body: Container(
+        child: SfCalendar(
+          view: CalendarView.week,
+        )
+      )
     );
   }
 }
